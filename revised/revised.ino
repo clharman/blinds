604bf66fc6510291570dab7e78308e5adac5f0d8
Colin Luongo Harman <clharman@umich.edu>
2015-02-07 19:30:26 -0500

    added shimmer to revised

--- revised/revised.ino
+++ revised/revised.ino
@@ -8,16 +8,36 @@
 
 //Colin Harman 01/27/15
 //Revisited and revised
+//  -Reworked overall structure
 //  -Simplified filter functions
 
 //Blinds project for Jim Cogswell, http://www.jimcogswell.com/
 
 ////////////////////////////////////////////
 //VARIABLE DECLARATIONS
+//All pin variables should be const
 
 //Potentiometer variables
-const int clockwiseThreshold = 140;      // point at which the blinds change direction from clockwise to anticlockwise. Ranges from 0 - 1023
-const int antiClockwiseThreshold = 850;  // point at which the blinds change direction from anticlockwise to clockwise
+const int pot_pin = 0;                   // Arduino pin for potentiometer input
+int pot_raw = 0;                         // potentiometer value storage
+const int lim_cw = 140;                  // point at which the blinds change direction from clockwise to anticlockwise. Ranges from 0 - 1023
+const int lim_ccw = 850;                 // point at which the blinds change direction from anticlockwise to clockwise
+int shim_cw_out = 275;                   // outside edge of clockwise shimmer threshold
+int shim_cw_in = 325;                    // inside edge of clockwise shimmer threshold
+int shim_ccw_out = 725;                  // outside edge of counterclockwise shimmer threshold
+int shim_ccw_in = 675;                   // inside edge of counterclockwise shimmer threshold
+
+//changes direction at each array level and proceeds to the next
+int limit_progression[] = {lim_cw, shim_cw_in, shim_cw_out, shim_ccw_out, shim_ccw_in, lim_ccw};
+
+
+
+//starts going clockwise until lim_cw
+int lim_next_index = 0;
+boolean motor_dir = true;                // direction of motor (true = clockwise) & initial direction
+
+
+
 
 //Microphone & signal variables
 int noise_thresh = 70;                   // threshold at which the microphone will begin listening. Ranges from 0 - 1023 - 75 seems to be optimal
@@ -30,16 +50,15 @@ float smoothedVal;                       // this holds the last loop value just 
 const int mic_pin = A5;                  // ?-> [the SPL output is connected to analog pin 0] <-?
 
 //Motor variables
-int motor_pin = 5;                       // Arduino pin for motor output
+const int motor_pin = 5;                 // Arduino pin for motor output
 int motor_spd = 0;                       // speed of motor (0 to 255)
-int relay_pwr_pin = 8;                   // Arduino pin for motor power (on/off) relay
-int relay_dir_pin = 10;                  // Arduino pin for motor direction relay
-int pot_pin = 0;                         // Arduino pin for potentiometer input
-int pot_raw = 0;                         // potentiometer value storage
-boolean motor_dir = true;                // direction of motor (true = clockwise)
+const int relay_pwr_pin = 8;             // Arduino pin for motor power (on/off) relay
+const int relay_dir_pin = 10;            // Arduino pin for motor direction relay
 int motor_scaled;                        // value storage for the scaled motor value
 int coast_down = 1000;                   // msecs to allow motor to stop
 int relay_delay = 10;                    // msecs to allow relay action
+
+
 
 
 //INIT
@@ -82,7 +101,6 @@ void loop(){
 
 ////////////////////////////////////////////
 //DIGITAL SMOOTHING FUNCTION
-
 int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray" passes an array to the function - the asterisk indicates the array name is a pointer
   int j, k, temp, top, bottom;
   long total;
@@ -90,16 +108,12 @@ int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray
   // static int raw[filter_size];
   static int sorted[filter_size];
   boolean done;
-
   i = (i + 1) % filter_size;    // increment counter and roll over if necc. -  % (modulo operator) rolls over variable
   sensSmoothArray[i] = rawIn;                 // input new data into the oldest slot
-
   // Serial.print("raw = ");
-
   for (j=0; j<filter_size; j++){     // transfer data array into anther array for sorting and averaging
     sorted[j] = sensSmoothArray[j];
   }
-
   done = 0;                // flag to know when we're done sorting              
   while(done != 1){        // simple swap sort, sorts numbers from lowest to highest
     done = 1;
@@ -112,7 +126,6 @@ int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray
       }
     }
   }
-
   /*
   for (j = 0; j < (filter_size); j++){    // print the array to debug
    Serial.print(sorted[j]); 
@@ -120,7 +133,6 @@ int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray
    }
    Serial.println();
    */
-
   // throw out top and bottom 25% of samples - limit to throw out at least one from top and bottom
   bottom = max(((filter_size * 25)  / 100), 1); 
   top = min((((filter_size * 75) / 100) + 1  ), (filter_size - 1));   // the + 1 is to make up for asymmetry caused by integer rounding
@@ -132,36 +144,26 @@ int digitalSmooth(int rawIn, int *sensSmoothArray){     // "int *sensSmoothArray
     // Serial.print(sorted[j]); 
     // Serial.print("   "); 
   }
-
   //  Serial.println();
   //  Serial.print("average = ");
   //  Serial.println(total/k);
   return total / k;    // divide by number of samples
 }
-
 ////////////////////////////////////////////
 //ALTERNATIVE SMOOTHING FUNCTION
-
 int smooth(int data, float filterVal, float smoothedVal){
-
-
   if (filterVal > 1){      // check to make sure param's are within range
     filterVal = .99;
   }
   else if (filterVal <= 0){
     filterVal = 0;
   }
-
   smoothedVal = (data * (1 - filterVal)) + (smoothedVal  *  filterVal);
-
   return (int)smoothedVal;
 }
-
-
 // reads mic pin
 // returns (0 or mic_raw)
-int micThreshed(){
-  
+int micThreshed(){ 
   mic_raw = analogRead(mic_pin);   // read sensor 1
   if(mic_raw > noise_thresh && ~(mic_raw > noise_loud && smoothedVal > 60)){ // if mic exceeds room noise BUT NOT if mic is loud while motor is high (ignores motor noise)
     return mic_raw;
@@ -170,14 +172,30 @@ int micThreshed(){
     return 0;
   }
 }
-
 void speedWrite(){
   smoothedVal = mic_smooth * 0.25; // divide the smoothed values by 4 so that they fit within a suitable output range for the motor
   analogWrite(motor_pin,smoothedVal); //set the motor to go at this speed
 }
 
-void directionWrite(){
-  if(pot_raw < clockwiseThreshold && motor_dir==true){ //if pot reads that its reached the edge and the motor is moving clockwise
+
+//returns true if:
+// - index mod 2 = 0 AND pot < limit
+// - index mod 2 = 1 AND pot > limit
+boolean potCompare(int pot_reading, int next_index){
+  if(next_index % 2 == 0 && pot_reading < limit_progression [next_index]){
+    return true;
+  }
+  else if(next_index % 2 == 1 && pot_reading > limit_progression [next_index]){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//changes motor direction
+void switchDirection(){
+  if(motor_dir==true){ //if pot reads that its reached the edge and the motor is moving clockwise
     digitalWrite(relay_pwr_pin, LOW); //turn off power
     delay(coast_down + relay_delay); //wait for it to slow down
     digitalWrite(relay_dir_pin,LOW); //change direction
@@ -185,7 +203,7 @@ void directionWrite(){
     digitalWrite(relay_pwr_pin,HIGH); //turn on motor
     motor_dir = false; //the direction is now anti-clockwise
   }
-  else if(pot_raw > antiClockwiseThreshold && motor_dir==false){ //if the pot reaches far left, and motor direction is anti-clockwise
+  else if(motor_dir==false){ //if the pot reaches far left, and motor direction is counterclockwise
     digitalWrite(relay_pwr_pin, LOW); //turn off power
     delay(coast_down + relay_delay); // wait for it to slow down
     digitalWrite(relay_dir_pin,HIGH); //change direction
@@ -195,11 +213,38 @@ void directionWrite(){
   }
 }
 
+//Writes motor direction using following algorithm:
+//NOTE: REQUIRES CORRECT INITIAL CONDITIONS TO FUNCTION.
+//IF INITIAL CONDITIONS ARE WRONG MECHANISM WILL NOT STOP
+//AT LIMITS AND MAY DAMAGE ITSELF
+void directionWrite(){
+  if(potCompare(pot_raw, lim_next_index)){
+      switchDirection();
+  }  
+  /*
+  if(pot_raw < lim_cw && motor_dir==true){ //if pot reads that its reached the edge and the motor is moving clockwise
+    digitalWrite(relay_pwr_pin, LOW); //turn off power
+    delay(coast_down + relay_delay); //wait for it to slow down
+    digitalWrite(relay_dir_pin,LOW); //change direction
+    delay(relay_delay);
+    digitalWrite(relay_pwr_pin,HIGH); //turn on motor
+    motor_dir = false; //the direction is now anti-clockwise
+  }
+  else if(pot_raw > lim_ccw && motor_dir==false){ //if the pot reaches far left, and motor direction is counterclockwise
+    digitalWrite(relay_pwr_pin, LOW); //turn off power
+    delay(coast_down + relay_delay); // wait for it to slow down
+    digitalWrite(relay_dir_pin,HIGH); //change direction
+    delay(relay_delay);
+    digitalWrite(relay_pwr_pin,HIGH); //turn on motor
+    motor_dir = true; //the direction is now clockwise
+  }*/
+}
+
+
 //Outputs debugging information in the serial window in the following format:
 // Uncomment the following Serial lines of code and open the serial monitor while running to see the raw mic value, the motor speed (smoothedVal), and potentiometer value
 //  mic_raw       motor_spd          POT VALUE  
 void debugSerial(int mic_raw, int smoothedVal, int pot_raw){
-
   Serial.print(mic_raw);
   Serial.print("           ");
   Serial.print(smoothedVal);
